c7a99ba51eec4b781c75fff1d2655c4cd89fa4d5
Humberto Colmenares <colmenareshr@gmail.com>
2023-06-11 16:57:35 -0300

    Initial Commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+const Navbar = () => {
+  return (
+    <header>
+      <div>
+        <img src="" alt="React Chat Logo" />
+      </div>
+      <nav>
+      <h1>Navbar</h1>
+      </nav>
+    </header>
+  )
+}
+
+export default Navbar
