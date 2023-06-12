818e13551108dccb983dba137d4b74bc73ebeed2
Humberto Colmenares <colmenareshr@gmail.com>
2023-06-12 00:22:29 -0300

    Create ChatBox component

--- src/components/ChatBox.jsx
+++ src/components/ChatBox.jsx
@@ -1,8 +1,45 @@
+import { useEffect, useState } from 'react';
+import {
+  query,
+  collection,
+  orderBy,
+  onSnapshot,
+  limit,
+} from 'firebase/firestore';
+import { db } from '../firebase';
+import Message from './Message';
+import SendMessage from './SendMessage';
 
 const ChatBox = () => {
-  return (
-    <div>ChatBox</div>
-  )
-}
+  const [messages, setMessages] = useState([])
 
-export default ChatBox
+  useEffect(() => {
+    const q = query(
+      collection(db, 'messages'),
+      orderBy('createdAt'),
+      limit(50)
+    );
+    const unsubscribe = onSnapshot(q, (QuerySnapshot) => {
+      let messages = [];
+      QuerySnapshot.forEach((doc) => {
+        messages.push({ ...doc.data(), id: doc.id });
+      });
+      setMessages(messages);
+    });
+    return () => unsubscribe;
+  }, []);
+  return (
+    <main>
+      <div>
+        {
+          messages?.map((message) => (
+            <Message key={messages.id} message={message} />
+          ))
+        }
+      </div>
+      <SendMessage />
+    </main>
+  );
+};
+
+export default ChatBox;
