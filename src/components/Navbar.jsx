664c21cead7049745fc34bd5d2c088d1c3fea043
Humberto Colmenares <colmenareshr@gmail.com>
2023-06-12 12:32:11 -0300

    traslate and fixing error

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -16,16 +16,16 @@ const [user] = useAuthState(auth);
     <header className='min-h-28 max-w-full py-6 bg-cyan-950 text-white  px-4'>
       <div className='container mx-auto flex items-center justify-between'>
         <div className='font-bold text-2xl md:text-3xl'>
-          <h1>React Chat</h1>
+          <span className='text-3xl font-extrabold md:text-4xl'>React Chat</span>
         </div>
         <div>
           <nav className='flex items-center gap-4'>
             {user ? (
-              <button onClick={signOut}>Logout</button>
+              <button onClick={signOut}>Log Out</button>
             ) : (
               <div onClick={googleSingIn} className='bg-white min-w-fit h-8 flex items-center cursor-pointer rounded-full px-4 shadow-2xl'>
                 <span className='flex item-center gap-3 justify-center text-black  '>
-                  <FcGoogle size={25} /> Login with Google
+                  <FcGoogle size={25} /> Log in with Google
                 </span>
               </div>
             )}
--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -8,17 +8,17 @@ const Welcome = () => {
     signInWithRedirect(auth, provider)
   };
   return (
-    <main className='bg-slate-300 w-full h-[100vh] flex flex-col items-center justify-center gap-2 text-center py-16'>
-      <h1>Bienvenido a tu App React Chat</h1>
+    <main className='bg-slate-300 w-full h-[100vh] flex flex-col items-center justify-center gap-3 text-center py-16'>
+      <h1>Welcome to your app React Chat</h1>
       <p>
-        Entra con tu cuenta de Goolge para chatear con otros desarrolladores
+        Enter your <strong>Google Account</strong> to chat with other developers.
       </p>
       <div
         onClick={googleSignIn}
         className='bg-white min-w-fit h-12 flex items-center cursor-pointer rounded-full px-6 shadow-2xl'
       >
         <span className='flex item-center gap-3 justify-center text-black font-semibold  '>
-          <FcGoogle size={30} /> Login with Google
+          <FcGoogle size={30} /> Log in with Google 
         </span>
       </div>
     </main>
