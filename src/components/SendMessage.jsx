7e93f31024bd8723b66aa0f8eaf9fb76f1c09821
Humberto Colmenares <colmenareshr@gmail.com>
2023-06-12 00:23:11 -0300

    create sendMessage component

--- src/components/SendMessage.jsx
+++ src/components/SendMessage.jsx
@@ -1,8 +1,43 @@
+import { auth, db } from '../firebase';
+import { addDoc, collection, serverTimestamp } from 'firebase/firestore';
+import { useState } from 'react';
 
 const SendMessage = () => {
-  return (
-    <div>SendMessage</div>
-  )
-}
+  const [message, setMessage] = useState();
 
-export default SendMessage
+  const sendMessage = async (e) => {
+    e.preventDefault();
+    if (message.trim === '') {
+      alert('hey! Envía un mensaje válido');
+      return;
+    }
+    const { uid, displayName, photoURL } = auth.currentUser;
+    await addDoc(collection(db, 'messages'), {
+      text: message,
+      name: displayName,
+      avatar: photoURL,
+      cerateAt: serverTimestamp(),
+      uid,
+    });
+    setMessage('');
+  };
+
+  return (
+    <form onSubmit={(e) => sendMessage(e)}>
+      <label htmlFor='messageInput' hidden>
+        Enter message
+      </label>
+      <input
+        value={message}
+        type='text'
+        id='messageInput'
+        name='messageInput'
+        placeholder='Type message'
+        onChange={(e) => setMessage(e.target.value)}
+      />
+      <button type='submit'>Send</button>
+    </form>
+  );
+};
+
+export default SendMessage;
