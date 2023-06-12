05cb3930bc760e4d18bfd056936a2cd118f5ba82
Humberto Colmenares <colmenareshr@gmail.com>
2023-06-12 11:43:18 -0300

    adding custom component and tailwind css style

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -1,8 +1,23 @@
+import { auth } from '../firebase';
+import { useAuthState } from 'react-firebase-hooks/auth';
 
-const Message = () => {
+const Message = ({ message }) => {
+  const [user] = useAuthState(auth);
   return (
-    <div>Message</div>
-  )
-}
+    <div className={`chat-bubble ${message.uid === user.uid ? 'right' : ''}`}>
+      <img
+        className='w-9 h-9 rounded-full mr-3'
+        src={message.avatar}
+        alt='user avatar'
+      />
+      <div className='chat-bubble__right'>
+        <p className='font-bold mb-1 text-base text-[#1c2c4c]'>
+          {message.name}
+        </p>
+        <p className='break-all'>{message.text}</p>
+      </div>
+    </div>
+  );
+};
 
-export default Message
+export default Message;
