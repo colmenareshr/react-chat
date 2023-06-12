b9720e610455f13acdd51dc619076749b7755149
Humberto Colmenares <colmenareshr@gmail.com>
2023-06-11 23:25:02 -0300

    adding login feat

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,14 +1,40 @@
+import { auth } from '../firebase';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { GoogleAuthProvider, signInWithRedirect } from 'firebase/auth';
+import { useState } from 'react';
+import { FcGoogle } from 'react-icons/fc';
 const Navbar = () => {
-  return (
-    <header>
-      <div>
-        <img src="" alt="React Chat Logo" />
-      </div>
-      <nav>
-      <h1>Navbar</h1>
-      </nav>
-    </header>
-  )
-}
+const [user] = useAuthState(auth);
 
-export default Navbar
+  const googleSingIn = () => {
+    const provider = new GoogleAuthProvider();
+    signInWithRedirect(auth, provider);
+  };
+  const signOut = () => {
+    auth.signOut();
+  };
+  return (
+    <header className='min-h-28 max-w-full py-6 bg-cyan-950 text-white  px-4'>
+      <div className='container mx-auto flex items-center justify-between'>
+        <div className='font-bold text-2xl md:text-3xl'>
+          <h1>React Chat</h1>
+        </div>
+        <div>
+          <nav className='flex items-center gap-4'>
+            {user ? (
+              <button onClick={signOut}>Logout</button>
+            ) : (
+              <div onClick={googleSingIn} className='bg-white min-w-fit h-8 flex items-center cursor-pointer rounded-full px-4 shadow-2xl'>
+                <span className='flex item-center gap-3 justify-center text-black  '>
+                  <FcGoogle size={25} /> Login with Google
+                </span>
+              </div>
+            )}
+          </nav>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Navbar;
