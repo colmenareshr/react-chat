48b7d78b8b5c02689b1147c7dd14f14d328f1c29
Humberto Colmenares <colmenareshr@gmail.com>
2023-06-12 18:38:01 -0300

    update env variables

--- src/firebase.js
+++ src/firebase.js
@@ -12,12 +12,12 @@ import { getFirestore } from 'firebase/firestore';
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 
 const firebaseConfig = {
-  apiKey: import.meta.env.PRODUCTION.VITE_API_KEY,
-  authDomain: import.meta.env.PRODUCTION.VITE_AUTH_DOMAIN,
-  projectId: import.meta.env.PRODUCTION.VITE_PROJECT_ID,
-  storageBucket: import.meta.env.PRODUCTION.VITE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.PRODUCTION.VITE_MESSSAGING_SENDER_ID,
-  appId: import.meta.env.PRODUCTION.VITE_APP_ID,
+  apiKey: import.meta.env.VITE_API_KEY,
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_APP_ID,
 };
 
 // Initialize Firebase
