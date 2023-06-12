ac9d70ea34abe828776a913d672ee6a7dcc9fbb4
Humberto Colmenares <colmenareshr@gmail.com>
2023-06-11 23:25:17 -0300

    conditional rendering

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,21 @@
-import './App.css'
+import { auth } from './firebase';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import Navbar from './components/Navbar';
+import Welcome from './components/Welcome'
+import ChatBox from './components/ChatBox'
 
 function App() {
+  const [user] = useAuthState(auth);
 
   return (
     <>
-        <h1>Hola Mundo</h1>
+      <Navbar />
+      {
+        !user ? <Welcome /> : <ChatBox />
+      }
+      <h1>Hola Mundo</h1>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
